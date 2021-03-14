2ac64f62dcae691e3bd11345c4f0c27202511472
MemicaC <corovic_selma@yahoo.com>
2021-03-14 18:27:35 +0100

    Treci dan

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  // Only change code below this line
+  // This is single line comment
+  /* This is multi-line comment
+    This is multi-line comment
+    This is multi-line comment
+    */
+  // Only change code above this line
+}
+console.log(main());
+module.exports = main;
